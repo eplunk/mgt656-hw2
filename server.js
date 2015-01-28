cf4a0ff3076e6d6e774c8918f064fe0eaad5c79f
eplunk <erplunkett@gmail.com>
2015-01-28 04:31:29 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello, Kyle and Miles');
+});
+
+app.get('/netid', function(request, response){
+    response.send('netiddddd');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('misty wolverine');
 });
 
 app.listen(process.env.PORT || 4000);
